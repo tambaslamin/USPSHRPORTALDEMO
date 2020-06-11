9f19abfd47e0b428db541c1b6da6c131b0e859ed
Nicholas Russell <nicholas.russell@accenture.com>
2020-06-11 19:34:53 -0400

    Initial commit of login page layout - Templates general login page via TWIG template customization - Custom module adds event subscriber to change login page title - Add "images" directory to theme for persistent theme image assets

---
+++ web/modules/custom/usps_hr/src/Routing/RouteSubscriber.php
@@ -0,0 +1,24 @@
+<?php
+namespace Drupal\usps_hr\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+
+    $entityUserEditFormRoute = $collection->get('user.login');
+
+    if ($entityUserEditFormRoute) {
+      $entityUserEditFormRoute->setDefaults([
+        '_form' => '\Drupal\user\Form\UserLoginForm',
+        '_title' => 'Employees login to the USPS HR Portal here',
+      ]);
+    }
+  }
+
+}
