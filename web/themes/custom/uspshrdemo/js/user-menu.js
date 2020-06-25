eaaab3c640f2ee0898e592e13dc05d93b30b38f0
Felipe Molina <a.molina.jimenez@accenture.com>
2020-06-25 13:50:16 -0600

    changing variable to directly usinig the dom elemen

--- web/themes/custom/uspshrdemo/js/user-menu.js
+++ web/themes/custom/uspshrdemo/js/user-menu.js
@@ -2,13 +2,12 @@
   Drupal.behaviors.myModuleBehavior = {
     attach: function (context, settings) {
       jQuery(document).ready(function() {
-        var $userMenu = jQuery('.user-menu-global');
         jQuery('.region-global-myaccount-top .field--name-user-picture', context).hover(
           function () {
-            $userMenu.show();//hover on
+            jQuery('.user-menu-global').show();//hover on
           },
           function () {
-            $userMenu.hide();//hover off
+            jQuery('.user-menu-global').hide();//hover off
           }
         );
       });
